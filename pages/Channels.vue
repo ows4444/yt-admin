75a0ca18dd17bb891cd4b162d6d5766a72ba51cb
ows4444 <ows4444@gmail.com>
2019-04-29 02:14:20 +0500

    asdf

--- pages/Channels.vue
+++ pages/Channels.vue
@@ -4,6 +4,14 @@ p List
 <script>
 export default {
     name: 'Channels',
-    meta: { admin: true }
+    meta: { admin: true },
+    async created() {
+        console.log('calling')
+        const ip = await this.$axios.$get(
+            'http://localhost:5000/youtube-player-238012/us-central1/api'
+        )
+
+        console.log(ip)
+    }
 }
 </script>
