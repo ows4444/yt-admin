ec25d3b9e49ba8c5de4da41e9948de1d4c8da240
ows4444 <ows4444@gmail.com>
2019-05-03 01:17:32 +0500

    fd

--- pages/Channels/Create.vue
+++ pages/Channels/Create.vue
@@ -1,4 +1,4 @@
-<template lang="pug">
+s<template lang="pug">
 
 .card 
     .card-header 
@@ -8,14 +8,24 @@
                 i.fas.fa-plus
     //.card-image 
     .card-content 
-        .field
-            p.control.has-icons-left.has-icons-right
+        .field.has-addons
+            p.control.has-icons-left.has-icons-right.is-expanded(:class="controls.video.isLoading?'is-loading':''")
+                input.input(type='text' v-model='videoId' placeholder='Video ID')
+                span.icon.is-small.is-left
+                    i.fab.fa-youtube.has-text-danger
+                span.icon.is-small.is-right 
+                    i(:class="typeof(controls.video.status)==='boolean'? (controls.video.status?'fas fa-check has-text-success':'fas fa-times has-text-danger') :''")
+            p.control
+                a.button(@click="LoadByVideoId(videoId)") Search
+        .field.has-addons
+            p.control.has-icons-left.has-icons-right.is-expanded(:class="controls.channel.isLoading?'is-loading':''")
                 input.input(type='text' v-model='channelId' placeholder='Channel ID')
                 span.icon.is-small.is-left
-                    i.fa.fa-play.has-text-danger
+                    i.fas.fa-tv.has-text-danger
                 span.icon.is-small.is-right
-                    //i.fas.fa-check.has-text-success
-                    //i.fas.fa-times.has-text-danger
+                    i(:class="typeof(controls.channel.status)==='boolean'?controls.channel.status?'fas fa-check has-text-success':'fas fa-times has-text-danger':''")
+            p.control
+                a.button(@click="LoadById(channelId)") Search
     .card-footer 
         .card-footer-item.button.is-info(@click="Reset") Reset
         .card-footer-item.button.is-primary(@click="Search") Search
@@ -30,19 +40,62 @@ export default {
     meta: { admin: true },
     data() {
         return {
+            controls: {
+                video: {
+                    isLoading: false,
+                    status: undefined
+                },
+                channel: {
+                    isLoading: false,
+                    status: undefined
+                }
+            },
             channelId: '',
+            videoId: '',
+            reset() {
+                this.channelId = ''
+                this.videoId = ''
+                this.data = null
+                this.controls.video = {
+                    isLoading: false,
+                    status: undefined
+                }
+                this.controls.channel = {
+                    isLoading: false,
+                    status: undefined
+                }
+            },
             data: {}
         }
     },
     methods: {
         Reset() {
-            this.channelId = ''
+            this.reset()
         },
         Search() {
             alert('Searching')
         },
         Create() {
             alert('Creating')
+        },
+        LoadById(id) {
+            const me = this
+            me.controls.channel.status = undefined
+            me.controls.channel.isLoading = true
+            setTimeout(() => {
+                me.controls.channel.isLoading = false
+                me.controls.channel.status = false
+            }, 2000)
+        },
+        LoadByVideoId(id) {
+            const me = this
+            me.controls.video.isLoading = true
+            me.controls.video.status = undefined
+            me.controls.video.isLoading = true
+            setTimeout(() => {
+                me.controls.video.isLoading = false
+                me.controls.video.status = false
+            }, 2000)
         }
     }
 }
